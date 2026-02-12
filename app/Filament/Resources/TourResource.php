c0ebb3bafc7a364a75de3e9effb1e3430e47ddce
Abburizal <aburizalchilliyat@gmail.com>
2026-02-13 01:12:05 +0700

    fix: Add missing FileUpload import to TourResource

--- app/Filament/Resources/TourResource.php
+++ app/Filament/Resources/TourResource.php
@@ -13,6 +13,7 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Components\FileUpload;
 
 class TourResource extends Resource
 {
